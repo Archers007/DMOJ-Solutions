6f04b01f58e4cefa51e8b0dd9fbbdad272f09f3f
Archers007 <45184743+Archers007@users.noreply.github.com>
2022-03-01 03:44:08 +0000

    to do list and new completed/gave up

--- README.md
+++ README.md
@@ -8,3 +8,30 @@
 2) IDK just like tell me if it doesent work </b>
 
 3) The folder called "Gave Up" are the ones i couldnt figure out at the time
+
+
+## Curently Working On:<br>
+#### [Mock CCC '19 Contest 2 J3 - Tudor Buys Some Tea](https://dmoj.ca/problem/nccc7j3)
+
+### To Do:
+
+1. Flying plushies
+2. Not enough users
+3. Vasile and haircut
+4. Pink
+5. Ren Ashbell
+6. An early Christamas present
+7. Discerning discounts
+8. Itami and Manga
+9. Kosnja
+10. Longest tunnel
+11. Pumpkin patches
+12. War on two fronts
+13. 16 bit
+14. Timsko
+16. A simplex problem
+17. Mimi and video games
+18. Shoes
+19. Love triangle
+20. What is n daddy?
+21. Faktor
--- workingFile.cpp
+++ workingFile.cpp
@@ -1,6 +1,7 @@
 //Includes-------------------------------------------
 #include <iostream>     //for using cout
 #include <cmath>//for math
+#include <math.h>
 #include <algorithm>
 #include <string>
 #include <cstring>
@@ -12,86 +13,27 @@ using namespace std;
 //Includes-------------------------------------------
 
 //var------------------------------------------------
-int casesnum=10;
-
-int cases[10]={1,2,3,4,5,6,7,8,9,10};
-
-int c1 = 100;
-int c2 = 500;
-int c3 = 1000;
-int c4 = 5000;
-int c5 = 10000;
-int c6 = 25000;
-int c7 = 50000;
-int c8 = 100000;
-int c9 = 500000;
-int c10 = 1000000;
-int bankers;
-int ins;
-
+int n,k;
 //var------------------------------------------------
 int main(){
-	cin>>ins;
+	cin>> n >>k;
 	
-	while(ins<=10){
-	
-	bool exists=find(std::begin(cases), std::end(cases), ins) != std::end(cases);
-		if(exists==1){
-		remove(std::begin(cases), std::end(cases), ins) != std::end(cases);
-			casesnum=casesnum-1;
-			}
-		
-		
-		cin>>ins;
-	}
-	if(ins>10){
-			bankers=ins;
-	}
-	int average=0;
-	int avgnum=0;
-	for(int i=0; i<casesnum; i++){
-		avgnum++;
-		if(cases[i]==1){
-			average=average+c1;
-		}
-		if(cases[i]==2){
-			average=average+c2;
-			}
-		if(cases[i]==3){
-			average=average+c3;
-			}
-		if(cases[i]==4){
-			average=average+c4;
-			}
-		if(cases[i]==5){
-			average=average+c5;
-			}
-		if(cases[i]==6){
-			average=average+c6;
-			}
-		if(cases[i]==7){
-			average=average+c7;
-			}
-		if(cases[i]==8){
-			average=average+c8;
-			}
-		if(cases[i]==9){
-			average=average+c9;
-			}
-		if(cases[i]==10){
-			average=average+c10;
-			}
-		
-	}
-	
-
-	int maybe=average/avgnum;
-	if(maybe<=bankers){
-		cout<<"deal";
-	}else if(bankers==98500){
-		cout<<"deal";
-	}else{
-		cout<<"no deal";
 	}
 
-	}
+
+/*Tudor, having made his own tea for far too long, has decided to go to a nearby tea shop to buy some tea.
+
+As every respectable tea shop does, this specific tea shop offers a loyalty program. For every  cups of tea that Tudor buys at the shop, the shop will give him one free cup of tea.
+
+Tudor wants to drink at least  cups of tea. How many cups of tea will he have to buy in order to achieve this goal?
+
+Constraints
+ - note that 32-bit integers may not be sufficient to solve this problem.
+
+In tests worth 5 marks, .
+
+Input Specification
+The input will consist of a single line containing two positive integers,  and .
+
+Output Specification
+Output, on a single line, the minimum number of cups of tea Tudor needs to buy.*/
